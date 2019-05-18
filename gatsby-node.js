87511edce2e925abaf598f60044fa6e2f4d4324c
Antoine Rousseau <antoine@rousseau.im>
2019-05-18 16:21:36 +0200

    add 404 test of createRemoteFileNode in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,22 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const { createRemoteFileNode } = require("gatsby-source-filesystem")
+
+exports.sourceNodes = ({ actions, store, cache, createNodeId }) => {
+  const { createNode } = actions
+
+  return createRemoteFileNode({
+    url: "https://httpstat.us/404",
+    store,
+    cache,
+    createNode,
+    createNodeId,
+  })
+  .catch(err => {
+    console.log("caught error:", err)
+  })
+  .then(node => {
+    console.log("created node:", node)
+  })
+}
